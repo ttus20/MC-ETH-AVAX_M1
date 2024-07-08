31f21e8eae8bf3b19a429f6e9aeaa1aae84fca18
Ghasutt <122854751+ttus20@users.noreply.github.com>
2024-07-08 22:16:37 +0800

    Update FunctionsAndErrors.sol

--- FunctionsAndErrors.sol
+++ FunctionsAndErrors.sol
@@ -1,34 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract ContractHandler {
-    uint public cBalance_;  // Contract Handler Balance
-    address public cHandler_; // Contract Handler User
+contract Email_Handling {
+    address public handler;
+    uint256 public mails;
 
+    event Deploy(address indexed sender, uint256 amount);
+    event Reply(address indexed recipient, uint256 amount);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    constructor() { 
-        cHandler_ = msg.sender;
-    
-    }
-
-    modifier contOwner_() { // Contract Owner (Handler)
-        require(cHandler_ == msg.sender, 
-            "User Handler is Unauthorized");
+    modifier onlyOwner() {
+        require(msg.sender == handler, "Handler is not the owner");
         _;
     }
 
-    function depoBalance_() //deposit value (funds)
-    public payable contOwner_ {
-        cBalance_ += msg.value;
+    constructor() {
+        handler = msg.sender;
     }
 
-    function widrawBalance(uint amount) //withdrawal value (funds)
-    public contOwner_ {
-        require(cBalance_ >= amount, 
-            "Insufficient Amount of Balance");
-           
-            cBalance_ -= amount;
+    function inbox() external payable {
+        require(msg.value > 0, "Inbox must be greater than zero");
+        mails += msg.value;
+        assert(mails >= msg.value);
+        emit Deploy(msg.sender, msg.value);
+    }
+
+    function sent (uint256 replied) external onlyOwner {
+        require(replied <= mails, "ERROR: Incorrect Amount");
+
+        mails -= replied;
+        payable(handler).transfer(replied);
+        emit Reply(handler, replied);
+    }
+
+    function changeHandler(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "New Owner, Handler is Changed");
+        emit OwnershipTransferred(handler, newOwner);
+        handler = newOwner;
+    }
+
+    function reviewed (uint256 open) external {
+        if (open > mails) {
+            revert("ERROR: Value exceeds current amount");
+        }
         
-        payable(cHandler_).transfer(amount);
+        mails -= open;
     }
 }
